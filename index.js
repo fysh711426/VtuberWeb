da48c243cf45bc015d7d5c32f28e78aa9c0583cd
fysh711426 <fysh711426@hotmail.com>
2022-07-25 13:54:37 +0800

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+var vm = new Vue({
+    el: '#app',
+    data: {
+        title: '',
+    },
+    methods: {
+        init: function () {
+            
+        },
+        getData: async function () {
+            
+        }
+    },
+    mounted() {
+        this.init();
+    }
+})
